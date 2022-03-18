2fd938bf68290f8b597a02573cda36d46b09ce72
Restuadil15 <73323256+Restuadil15@users.noreply.github.com>
2022-03-18 21:38:38 +0700

    Add files via upload

---
+++ src/components/Main.js
@@ -0,0 +1,14 @@
+import Hello from "./Hello";
+const Main = () => {
+    return (
+        <main>
+            <Hello name="Aufa" />
+            <Hello name="Restu"/>
+            <Hello name="Adil"/>
+            <Hello name="Salatin"/>
+            <Hello name="Syah"/>
+        </main>
+    );
+};
+
+export default Main ; 
